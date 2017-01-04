00d3a8f2bada9fc99d0a1ba41e884a027e03357f
Federico Lo Grasso <lograssofede@gmail.com>
2017-01-04 15:53:03 -0300

    Changes

--- Gateway_SMS/MainForm.Designer.cs
+++ Gateway_SMS/MainForm.Designer.cs
@@ -14,6 +14,28 @@ namespace Gateway_SMS
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel_conexion;
+		private System.Windows.Forms.TextBox textBox_IMEI;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button_conectar;
+		private System.Windows.Forms.Label label_estadoConexion;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox_port;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel_estado;
+		private System.Windows.Forms.Label SMSReady_label;
+		private System.Windows.Forms.Label CSQ_label;
+		private System.Windows.Forms.Label AT_label;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button_verSMS;
+		private System.Windows.Forms.Panel panel_verSMS;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+
+
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -33,15 +55,236 @@ namespace Gateway_SMS
 		/// This method is required for Windows Forms designer support.
 		/// Do not change the method contents inside the source code editor. The Forms designer might
 		/// not be able to load this method if it was changed manually.
-		/// </summary>
 		private void InitializeComponent()
 		{
+			this.panel_conexion = new System.Windows.Forms.Panel();
+			this.textBox_IMEI = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button_conectar = new System.Windows.Forms.Button();
+			this.label_estadoConexion = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox_port = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel_estado = new System.Windows.Forms.Panel();
+			this.SMSReady_label = new System.Windows.Forms.Label();
+			this.CSQ_label = new System.Windows.Forms.Label();
+			this.AT_label = new System.Windows.Forms.Label();
+			this.panel_verSMS = new System.Windows.Forms.Panel();
+			this.button_verSMS = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.panel_conexion.SuspendLayout();
+			this.panel_estado.SuspendLayout();
+			this.panel_verSMS.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel_conexion
+			// 
+			this.panel_conexion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel_conexion.Controls.Add(this.textBox_IMEI);
+			this.panel_conexion.Controls.Add(this.label5);
+			this.panel_conexion.Controls.Add(this.button_conectar);
+			this.panel_conexion.Controls.Add(this.label_estadoConexion);
+			this.panel_conexion.Controls.Add(this.label3);
+			this.panel_conexion.Controls.Add(this.textBox_port);
+			this.panel_conexion.Controls.Add(this.label2);
+			this.panel_conexion.Controls.Add(this.label1);
+			this.panel_conexion.Location = new System.Drawing.Point(12, 12);
+			this.panel_conexion.Name = "panel_conexion";
+			this.panel_conexion.Size = new System.Drawing.Size(306, 92);
+			this.panel_conexion.TabIndex = 1;
+			// 
+			// textBox_IMEI
+			// 
+			this.textBox_IMEI.Location = new System.Drawing.Point(57, 50);
+			this.textBox_IMEI.Name = "textBox_IMEI";
+			this.textBox_IMEI.ReadOnly = true;
+			this.textBox_IMEI.Size = new System.Drawing.Size(122, 20);
+			this.textBox_IMEI.TabIndex = 7;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(12, 50);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(57, 23);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "IMEI:";
+			// 
+			// button_conectar
+			// 
+			this.button_conectar.Location = new System.Drawing.Point(101, 24);
+			this.button_conectar.Name = "button_conectar";
+			this.button_conectar.Size = new System.Drawing.Size(75, 23);
+			this.button_conectar.TabIndex = 5;
+			this.button_conectar.Text = "Conectar";
+			this.button_conectar.UseVisualStyleBackColor = true;
+			this.button_conectar.Click += new System.EventHandler(this.Button_conectarClick);
+			// 
+			// label_estadoConexion
+			// 
+			this.label_estadoConexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label_estadoConexion.Location = new System.Drawing.Point(185, 38);
+			this.label_estadoConexion.Name = "label_estadoConexion";
+			this.label_estadoConexion.Size = new System.Drawing.Size(110, 23);
+			this.label_estadoConexion.TabIndex = 4;
+			this.label_estadoConexion.Text = "DESCONECTADO";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(185, 21);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 23);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Estado:";
+			// 
+			// textBox_port
+			// 
+			this.textBox_port.Location = new System.Drawing.Point(54, 24);
+			this.textBox_port.Name = "textBox_port";
+			this.textBox_port.Size = new System.Drawing.Size(41, 20);
+			this.textBox_port.TabIndex = 2;
+			this.textBox_port.Text = "COM5";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 27);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(57, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Puerto:";
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(4, 4);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(94, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Conexion SIM:";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// panel_estado
+			// 
+			this.panel_estado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel_estado.Controls.Add(this.SMSReady_label);
+			this.panel_estado.Controls.Add(this.CSQ_label);
+			this.panel_estado.Controls.Add(this.AT_label);
+			this.panel_estado.Location = new System.Drawing.Point(13, 182);
+			this.panel_estado.Name = "panel_estado";
+			this.panel_estado.Size = new System.Drawing.Size(306, 93);
+			this.panel_estado.TabIndex = 2;
+			this.panel_estado.Visible = false;
+			// 
+			// SMSReady_label
+			// 
+			this.SMSReady_label.Location = new System.Drawing.Point(0, 65);
+			this.SMSReady_label.Name = "SMSReady_label";
+			this.SMSReady_label.Size = new System.Drawing.Size(179, 23);
+			this.SMSReady_label.TabIndex = 3;
+			// 
+			// CSQ_label
+			// 
+			this.CSQ_label.Location = new System.Drawing.Point(0, 42);
+			this.CSQ_label.Name = "CSQ_label";
+			this.CSQ_label.Size = new System.Drawing.Size(135, 23);
+			this.CSQ_label.TabIndex = 2;
+			// 
+			// AT_label
+			// 
+			this.AT_label.Location = new System.Drawing.Point(0, 19);
+			this.AT_label.Name = "AT_label";
+			this.AT_label.Size = new System.Drawing.Size(135, 23);
+			this.AT_label.TabIndex = 0;
+			// 
+			// panel_verSMS
+			// 
+			this.panel_verSMS.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel_verSMS.Controls.Add(this.button_verSMS);
+			this.panel_verSMS.Controls.Add(this.label8);
+			this.panel_verSMS.Controls.Add(this.label4);
+			this.panel_verSMS.Controls.Add(this.label6);
+			this.panel_verSMS.Controls.Add(this.label7);
+			this.panel_verSMS.Location = new System.Drawing.Point(13, 110);
+			this.panel_verSMS.Name = "panel_verSMS";
+			this.panel_verSMS.Size = new System.Drawing.Size(306, 66);
+			this.panel_verSMS.TabIndex = 5;
+			// 
+			// button_verSMS
+			// 
+			this.button_verSMS.Location = new System.Drawing.Point(104, 19);
+			this.button_verSMS.Name = "button_verSMS";
+			this.button_verSMS.Size = new System.Drawing.Size(75, 23);
+			this.button_verSMS.TabIndex = 5;
+			this.button_verSMS.Text = "Ver SMS";
+			this.button_verSMS.UseVisualStyleBackColor = true;
+			this.button_verSMS.Click += new System.EventHandler(this.Button_verSMSClick);
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(4, 8);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(86, 23);
+			this.label8.TabIndex = 4;
+			this.label8.Text = "SMS:";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(0, 65);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(179, 23);
+			this.label4.TabIndex = 3;
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(0, 42);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(135, 23);
+			this.label6.TabIndex = 2;
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(0, 19);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(135, 23);
+			this.label7.TabIndex = 0;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(325, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.Size = new System.Drawing.Size(342, 338);
+			this.dataGridView1.TabIndex = 3;
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Gateway_SMS";
+			this.ClientSize = new System.Drawing.Size(324, 285);
+			this.Controls.Add(this.panel_verSMS);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.panel_estado);
+			this.Controls.Add(this.panel_conexion);
 			this.Name = "MainForm";
+			this.Text = "Gateway_SMS";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFormFormClosed);
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.panel_conexion.ResumeLayout(false);
+			this.panel_conexion.PerformLayout();
+			this.panel_estado.ResumeLayout(false);
+			this.panel_verSMS.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 	}
 }
--- Gateway_SMS/MainForm.cs
+++ Gateway_SMS/MainForm.cs
@@ -8,8 +8,15 @@
  */
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
 
 namespace Gateway_SMS
 {
@@ -18,16 +25,464 @@ namespace Gateway_SMS
 	/// </summary>
 	public partial class MainForm : Form
 	{
+		/*Creo clases*/
+		SIM900 sim900;
+		SerialPort serialPort1;
+		DBconnection dbConnection;
+		DataTable dt;
+		
 		public MainForm()
 		{
 			//
 			// The InitializeComponent() call is required for Windows Forms designer support.
 			//
 			InitializeComponent();
-			//Test2
+			
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		
+		void SerialPort1DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+			sim900.setReceivedBuffer(sim900.getReceivedBuffer()+serialPort1.ReadLine());
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			dataGridView1.Visible=false;
+			dbConnection = new DBconnection();
+			this.Size = new Size(340, 220);
+			serialPort1 = new SerialPort();
+			sim900 = new SIM900(ref serialPort1);
+			this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1DataReceived);
+		}
+		void MainFormFormClosed(object sender, FormClosedEventArgs e)
+		{
+			try{
+				if(serialPort1.IsOpen){
+					serialPort1.Close();
+				}
+				
+			}
+			catch (Exception ex){}
+		}
+		void Button_conectarClick(object sender, EventArgs e)
+		{
+			bool OK= true;
+
+			label_estadoConexion.Text="CONECTANDO...";
+			panel_conexion.Refresh();
+			
+			try{
+				serialPort1.Close();
+				serialPort1.PortName=textBox_port.Text;
+				serialPort1.Open();
+			}
+			catch(Exception ex){
+				MessageBox.Show("Error de Conexión con el módulo SIM","ERROR",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+			}
+			
+			if (serialPort1.IsOpen){
+				
+				label_estadoConexion.Text="CONECTADO";
+				
+				/*Bajo el panel de ver SMS*/
+				panel_verSMS.Location=new Point(12,209);
+				
+				/*Subo el panel de estado*/
+				panel_estado.Location=new Point(12,110);
+				
+				panel_estado.Show();
+				this.Size = new Size(340, 250);
+				
+				
+				
+				/*Intento conectarme al SIM*/
+				if(sim900.connectSIM900()){
+					AT_label.Text="SIM900 ONLINE";
+				}
+				else
+				{
+					AT_label.Text="SIM900 OFFLINE";
+					OK=false;
+				}
+				
+				/*Intento adquirir IMEI*/
+				if(sim900.setIMEI()){
+					textBox_IMEI.Text=sim900.getIMEI();
+				}
+				else
+				{
+					textBox_IMEI.Text="IMEI ERROR";
+					OK=false;
+				}
+				
+				/*Intento adquirir señal*/
+				if (sim900.setSignal()){
+					CSQ_label.Text="SEÑAL:"+sim900.getSignal();
+				}
+				else
+				{
+					CSQ_label.Text="ERROR EN SEÑAL";
+					OK=false;
+				}
+				
+				/*Intento setear modo SMS*/
+				if (sim900.prepareSMS()){
+					SMSReady_label.Text="LISTO PARA ENVIAR SMS";
+				}
+				else
+				{
+					SMSReady_label.Text="NO LISTO PARA ENVIAR SMS";
+					OK=false;
+				}
+				
+				panel_estado.Refresh();
+				
+				/*Todo OK*/
+				
+				if(OK){
+
+					/*Desactivo boton conectar*/
+					button_conectar.Enabled=false;
+					
+					/*Deshabilito textbox puerto*/
+					textBox_port.Enabled=false;
+					
+					/*Oculto boton de "Ver SMS"*/
+					panel_verSMS.Visible=false;
+					
+
+					/*Agrando para ver el dataGrid*/
+					this.Size = new Size(690, 400);
+					
+					/*Seteo parametros*/
+					dbConnection.setdatabaseName("testcsharp");
+					dbConnection.setServer("localhost");
+					dbConnection.setdatabaseUser("root");
+					dbConnection.setdatabasePassword("");
+					
+					/*Me conecto a la DB*/
+					if (dbConnection.DBConnect()){
+						
+						/*Ejecuto query*/
+						dataGridView1.DataSource=dbConnection.executeQuery("SELECT date,number,message,state FROM `sms`");
+						
+						/*Muestro resultados*/
+						dataGridView1.Columns[0].HeaderText="Fecha";
+						dataGridView1.Columns[1].HeaderText="Numero";
+						dataGridView1.Columns[2].HeaderText="Mensaje";
+						dataGridView1.Columns[3].HeaderText="Estado";
+						dataGridView1.AutoResizeColumns();
+						dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+						dataGridView1.Visible=true;
+					}
+					
+				}
+				/*Si no estoy listo para mandar SMS*/
+				else
+				{	
+					/*Muestro panel estado y panel ver SMS*/
+					this.Size = new Size(340, 323);
+					
+				}
+
+
+			}
+			
+			/*Puerto serial no abierto*/
+			else
+			{
+				label_estadoConexion.Text="DESCONECTADO";
+				panel_estado.Hide();
+
+				
+			}
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		void Button_verSMSClick(object sender, EventArgs e)
+		{
+			/*Agrando para ver el dataGrid*/
+			this.Size = new Size(690, 400);
+			
+			/*Seteo parametros*/
+			dbConnection.setdatabaseName("testcsharp");
+			dbConnection.setServer("localhost");
+			dbConnection.setdatabaseUser("root");
+			dbConnection.setdatabasePassword("");
+			
+			/*Me conecto a la DB*/
+			if (dbConnection.DBConnect()){
+				
+				/*Ejecuto query*/
+				dataGridView1.DataSource=dbConnection.executeQuery("SELECT date,number,message,state FROM `sms`");
+				
+				/*Muestro resultados*/
+				dataGridView1.Columns[0].HeaderText="Fecha";
+				dataGridView1.Columns[1].HeaderText="Numero";
+				dataGridView1.Columns[2].HeaderText="Mensaje";
+				dataGridView1.Columns[3].HeaderText="Estado";
+				dataGridView1.AutoResizeColumns();
+				dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+				dataGridView1.Visible=true;
+			}
+			
+		}
+	}
+	
+	public class DBconnection{
+		
+		private MySqlConnection conexion;
+		private DataSet ds;
+		private MySqlDataAdapter adapter;
+		
+		private string server;
+		private string databaseName;
+		private string databaseUser;
+		private string databasePassword;
+		
+		public DBconnection(){
+			ds = new DataSet();
+		}
+		
+		
+		
+		public void setServer(string server){
+			this.server=server;
+		}
+		
+		public void setdatabaseName(string databaseName){
+			this.databaseName=databaseName;
+		}
+		
+		public void setdatabaseUser(string databaseUser){
+			this.databaseUser=databaseUser;
+		}
+		
+		public void setdatabasePassword(string databasePassword){
+			this.databasePassword=databasePassword;
+		}
+		
+
+		public bool DBConnect(){
+			
+			string conexionString = "server="+server+";database="+databaseName+";user="+databaseUser+";password="+databasePassword+";";
+			conexion = new MySqlConnection(conexionString);
+			
+			try
+			{
+				conexion.Open();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			
+			return (conexion.State==ConnectionState.Open);
+			
+		}
+		public DataTable executeQuery(string query){
+			
+			adapter= new MySqlDataAdapter(query,conexion);
+			
+			try
+			{
+				adapter.Fill(ds,"Phone_Table");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			adapter = null;
+			return ds.Tables["Phone_Table"];
+			
+		}
+	}
+	
+	public class SIM900{
+		
+		private String receivedBuffer;
+		private string IMEI;
+		private string signal;
+		private SerialPort serialPort;
+		
+		/*Constructor*/
+		public SIM900(ref SerialPort serial){
+			receivedBuffer="";
+			IMEI="";
+			signal = "";
+			serialPort=serial;
+		}
+		
+		public String getSignal(){
+			return signal;
+		}
+		
+		public String getReceivedBuffer(){
+			return receivedBuffer;
+		}
+		
+		public void setReceivedBuffer(String receivedBuffer){
+			this.receivedBuffer=receivedBuffer;
+		}
+		
+		public String getIMEI(){
+			return IMEI;
+		}
+		
+		public void waitForResult(String response){
+			while((getReceivedBuffer().IndexOf(response)==-1)){}
+		}
+		
+		
+		/*Return = null --> Comando falló
+		 *Return !=null 	--> Comando OK*/
+		String sendCommand(String command, int delaySec, String response){
+			
+			string received = "";
+			receivedBuffer="";
+			
+			/*Envio Comando*/
+			serialPort.WriteLine((command+"\r\n"));
+			
+			/*Espero hasta que pase el tiempo delaySec o encuentre el string response en la respuesta*/
+			var task = Task.Run(() => waitForResult(response));
+			if (task.Wait(TimeSpan.FromSeconds(delaySec))){
+				/*Encontro string*/
+				received=receivedBuffer;
+			}
+			else
+			{
+				/*No Encontro string*/
+				received = null;
+			}
+			
+			receivedBuffer="";
+			return received;
+		}
+		
+		public bool enviarSMS(string numero, string mensaje){
+			bool OK=true;
+			
+			
+			if ((sendCommand("AT+CMGS=\""+numero+"\"",4,"ERROR")) != null){
+				OK=false;
+			}
+			
+			if ((sendCommand(mensaje,4,"ERROR")) != null){
+				OK=false;
+			}
+			
+			if ((sendCommand("\x1A",3,"OK")) == null){
+				OK=false;
+			}
+			
+			return OK;
+
+		}
+		
+		public bool connectSIM900(){
+			
+			String result = "";
+			bool OK = true;
+			
+			
+			if (sendCommand("ATE0",2,"OK")==null){
+				OK = false;
+			}
+			
+			if ((result = sendCommand("AT+CREG?",2,"OK")) != null){
+				if ((result.IndexOf("0,1")==-1)&&(result.IndexOf("1,1")==-1)){
+					OK=false;
+				}
+			}
+			else
+			{
+				OK=false;
+			}
+
+			
+			if (sendCommand("AT+CPIN?",2,"OK")==null){
+				OK=false;
+			}
+			
+			return OK;
+		}
+		
+		
+		public bool setIMEI(){
+			
+			string result="";
+			bool OK = true;
+			
+			if ((result = sendCommand("AT+CGSN",2,"OK")) != null){
+				IMEI=result.Substring(0,result.Length-3);
+			}
+			else
+			{
+				OK=false;
+			}
+			
+			return OK;
+			
+		}
+
+		
+		
+		public bool setSignal(){
+			
+			string result = "";
+			bool OK = true;
+			
+			if ((result = sendCommand("AT+CSQ",2,"OK")) != null){
+				
+				int i =0;
+				
+				
+				do{
+					i++;
+				}while ((result[i] != ':') && ( i<result.Length));
+				
+				if (Char.IsNumber(result[i+2]) && Char.IsNumber(result[i+3])){
+					signal=(Convert.ToString(result[i+2]))+(Convert.ToString(result[i+3]));
+
+				}
+				else
+					if (Char.IsNumber(result[i+2])){
+					signal="0"+result[i+2];
+					
+				}
+
+			}
+			else
+			{
+				OK=false;
+				
+			}
+			return OK;
+			
+		}
+		
+		public bool prepareSMS(){
+			
+			bool OK = true;
+			
+			if ((sendCommand("AT+CMGF=1",2,"OK")) == null){
+				OK=false;
+			}
+			
+			if (sendCommand("AT+CSCS=\"GSM\"",2,"OK") == null){
+				OK=false;
+			}
+			
+			
+			return OK;
+			
+		}
+
 	}
 }

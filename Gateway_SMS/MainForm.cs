3f21166ccd3432f617d68be5f637f4eec4a49275
Federico Lo Grasso <lograssofede@gmail.com>
2017-01-04 10:40:33 -0300

    Add credentials GIT

--- Gateway_SMS/MainForm.cs
+++ Gateway_SMS/MainForm.cs
@@ -24,7 +24,7 @@ namespace Gateway_SMS
 			// The InitializeComponent() call is required for Windows Forms designer support.
 			//
 			InitializeComponent();
-			
+			//Test
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
